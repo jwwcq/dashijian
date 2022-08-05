4ae7d4fc9b638163a4af58f49a97f26cb8b63ee8
jwwcq <1461206455@qq.com>
2022-08-05 10:37:09 +0800

    文章增删改完成

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -17,9 +17,10 @@ initArtCateList()
 
 const layer = layui.layer
 const form = layui.form
+let layerAdd = null
 
 $('#addCateBtn').click(function () {
-    layer.open({
+    layerAdd = layer.open({
         type: 1,
         area: ["500px", "250px"],
         title: "添加文章分类",
@@ -34,7 +35,66 @@ $('body').on('submit', '#form-add', function (e) {
         url: '/my/article/addcates',
         data: form.val('formAdd'),
         success: res => {
-            console.log(res);
+            const { status, message } = res
+            layer.msg(message)
+            if (status !== 0) return
+            initArtCateList()
+            layer.close(layerAdd)
         }
     })
+})
+
+let layerEdit = null
+$('#tb').on('click', '.btn-edit', function () {
+    layerEdit = layer.open({
+        type: 1,
+        area: ['500px', '250px'],
+        title: "添加文章分类",
+        content: $('#dialog-edit').html(),
+    })
+    let id = $(this).attr('data-id')
+    $.ajax({
+        type: 'GET',
+        url: '/my/article/cates/' + id,
+        success: res => {
+            const { status, message, data } = res
+            if (status !== 0) return layer.msg(message)
+            form.val('formEdit', data)
+        }
+    })
+})
+
+$('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault()
+    $.ajax({
+        type: 'POST',
+        url: '/my/article/updatecate',
+        data: form.val('formEdit'),
+        success: res => {
+            const { status, message } = res
+            layer.msg(message)
+            if (status !== 0) return
+            layer.close(layerEdit);
+            initArtCateList()
+
+        }
+    })
+})
+
+$('#tb').on('click', '.btn-delete', function () {
+    let id = $(this).attr('data-id')
+    layer.confirm('确定删除?', { icon: 3, title: '提示' }, function (index) {
+        $.ajax({
+            type: 'GET',
+            url: '/my/article/deletecate/' + id,
+            data: null,
+            success: res => {
+                const { status, message } = res
+                layer.msg(message)
+                if (status !== 0) return
+                initArtCateList()
+            }
+        })
+        layer.close(index);
+    });
 })
